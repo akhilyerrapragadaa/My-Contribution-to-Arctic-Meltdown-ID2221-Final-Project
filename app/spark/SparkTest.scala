db3020cd52bd506bb593c019aa2581057bd68e56
akhilyerrapragadaa <akhilyerrapragada905@gmail.com>
2020-10-14 23:08:26 +0200

    scala code init

--- app/spark/SparkTest.scala
+++ app/spark/SparkTest.scala
@@ -9,6 +9,17 @@ object SparkTest {
 
     val sparkS = SparkSession.builder.master("local[4]").getOrCreate
     import sparkS.implicits._
+    import org.apache.spark.sql.functions._
+
+    // Create a DataFrame based on the JSON results.
+    val fileName = "/home/nanda/allData/methane.json"
+    val df = sparkS.read.json(fileName)  
+
+    val content = df.select(explode($"methane").alias("new_batter"))
+
+    val bat = content.select("new_batter.*")
+
+    val sam = bat.rdd.map(x => (x(2) ,x(0)))
 
     val sum = Seq(3, 2, 4, 1, 0, 30,30,40,50,-4).toDS
     sum.count.toInt
