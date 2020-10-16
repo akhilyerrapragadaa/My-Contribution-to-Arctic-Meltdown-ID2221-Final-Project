b59a1d794c73ac698ab6892e2ec8ed25ee96222b
akhilyerrapragadaa <akhilyerrapragada905@gmail.com>
2020-10-16 22:12:10 +0200

    Endpoints added

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -8,22 +8,39 @@ import scala.concurrent.ExecutionContext.Implicits._
 // Spark
 import spark.SparkTest
 
-
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
 
-  // A simple example to call Apache Spark
-  def test = Action { implicit request =>
-  	val sum = SparkTest.Example
+  //  Methane Emissions end point
+  def Methane = Action { implicit request =>
+  	val sum = SparkTest.MethaneEmissions
+    Ok(views.html.test_args(s"A call to Spark, with result: $sum"))
+  }
+
+  // NO Emissions end point
+  def NOEmissions = Action { implicit request =>
+  	val sum = SparkTest.NOEmissions
+    Ok(views.html.test_args(s"A call to Spark, with result: $sum"))
+  }
+
+  // PolarIce end point
+  def PolarIce = Action { implicit request =>
+  	val sum = SparkTest.PolarIce
+    Ok(views.html.test_args(s"A call to Spark, with result: $sum"))
+  }
+
+  // Temperature end point
+  def Temperature = Action { implicit request =>
+  	val sum = SparkTest.Temperature
     Ok(views.html.test_args(s"A call to Spark, with result: $sum"))
   }
 
   // A non-blocking call to Apache Spark 
   def testAsync = Action.async{
-  	val futureSum = Future{SparkTest.Example}
+  	val futureSum = Future{SparkTest.MethaneEmissions}
     futureSum.map{ s => Ok(views.html.test_args(s"A non-blocking call to Spark with result: ${s + 1000}"))}
   }
 
--- app/spark/SparkTest.scala
+++ app/spark/SparkTest.scala
@@ -5,14 +5,14 @@ import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.cassandra._
 import com.datastax.spark.connector._
 import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
+import org.apache.spark.sql.functions._
 
 object SparkTest {
 
-  def Example : Int = {
+  def MethaneEmissions : Int = {
 
     val sparkS = SparkSession.builder.master("local[4]").getOrCreate
     import sparkS.implicits._
-    import org.apache.spark.sql.functions._
 
      val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
      val session = cluster.connect()
@@ -38,4 +38,95 @@ object SparkTest {
     sum.count.toInt
 
   }
+
+   def NOEmissions : Int = {
+
+    val sparkS = SparkSession.builder.master("local[4]").getOrCreate
+    import sparkS.implicits._
+
+     val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+     val session = cluster.connect()
+
+     session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+     session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
+
+    // Create a DataFrame based on the JSON results.
+    val fileName = "/home/nanda/allData/NO.json"
+    val df = sparkS.read.json(fileName)  
+
+    val content = df.select(explode($"nitrous").alias("new_batter"))
+
+    val bat = content.select("new_batter.*")
+
+    val sam = bat.rdd.map(x => (x(2) ,x(0)))
+
+    sam.saveToCassandra("avg_space", "avg", SomeColumns("word", "count"))
+
+    sam.take(10).foreach(println)
+
+    val sum = Seq(3, 2, 4, 1, 0, 30,30,40,50,-4).toDS
+    sum.count.toInt
+
+  }
+
+  def PolarIce : Int = {
+
+    val sparkS = SparkSession.builder.master("local[4]").getOrCreate
+    import sparkS.implicits._
+
+     val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+     val session = cluster.connect()
+
+     session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+     session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
+
+    // Create a DataFrame based on the JSON results.
+    val fileName = "/home/nanda/allData/PolarIce.json"
+    val df = sparkS.read.json(fileName)  
+
+    val content = df.select(explode($"result").alias("new_batter"))
+
+    val bat = content.select("new_batter.*")
+
+    val sam = bat.rdd.map(x => (x(2) ,x(0)))
+
+    sam.saveToCassandra("avg_space", "avg", SomeColumns("word", "count"))
+
+    sam.take(10).foreach(println)
+
+    val sum = Seq(3, 2, 4, 1, 0, 30,30,40,50,-4).toDS
+    sum.count.toInt
+
+  }
+
+  def Temperature : Int = {
+
+    val sparkS = SparkSession.builder.master("local[4]").getOrCreate
+    import sparkS.implicits._
+
+     val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+     val session = cluster.connect()
+
+     session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+     session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
+
+    // Create a DataFrame based on the JSON results.
+    val fileName = "/home/nanda/allData/Temperature.json"
+    val df = sparkS.read.json(fileName)  
+
+    val content = df.select(explode($"result").alias("new_batter"))
+
+    val bat = content.select("new_batter.*")
+
+    val sam = bat.rdd.map(x => (x(2) ,x(0)))
+
+    sam.saveToCassandra("avg_space", "avg", SomeColumns("word", "count"))
+
+    sam.take(10).foreach(println)
+
+    val sum = Seq(3, 2, 4, 1, 0, 30,30,40,50,-4).toDS
+    sum.count.toInt
+
+  }
+
 }
