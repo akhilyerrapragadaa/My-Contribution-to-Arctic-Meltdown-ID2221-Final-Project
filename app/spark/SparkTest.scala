3782a6f46bb4d82d009db9079db6e27491426ddb
akhilyerrapragadaa <akhilyerrapragada905@gmail.com>
2020-10-16 21:12:17 +0200

    cassandra integration

--- app/spark/SparkTest.scala
+++ app/spark/SparkTest.scala
@@ -2,6 +2,9 @@ package spark
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.cassandra._
+import com.datastax.spark.connector._
+import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
 
 object SparkTest {
 
@@ -10,6 +13,12 @@ object SparkTest {
     val sparkS = SparkSession.builder.master("local[4]").getOrCreate
     import sparkS.implicits._
     import org.apache.spark.sql.functions._
+
+     val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+     val session = cluster.connect()
+
+     session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+     session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
 
     // Create a DataFrame based on the JSON results.
     val fileName = "/home/nanda/allData/methane.json"
@@ -20,6 +29,10 @@ object SparkTest {
     val bat = content.select("new_batter.*")
 
     val sam = bat.rdd.map(x => (x(2) ,x(0)))
+
+    sam.saveToCassandra("avg_space", "avg", SomeColumns("word", "count"))
+
+    sam.take(10).foreach(println)
 
     val sum = Seq(3, 2, 4, 1, 0, 30,30,40,50,-4).toDS
     sum.count.toInt
